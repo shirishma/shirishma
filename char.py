6cfe10d79dca06fbbe82236d71dace6836137aa7
shirishma <51737901+shirishma@users.noreply.github.com>
2019-07-18 21:55:03 +0530

    Update char.py

--- char.py
+++ char.py
@@ -1,5 +1,12 @@
-a=['a','e','i','o','u']
-if i in a:
-    print("Vowel")
+a=input()
+if(a>='a' and a<='z') or(a>='A' or a<='Z'):
+    if(a='a' or a='A' or a='e' or a='E' or a='i' or a='I' or a='o' or a='O' or a='u' or a='U'):
+        print("Vowel")
+    else:
+        print("Consonant")
 else:
-    print("Consonant")
+    print("Invalid")
+        
+        
+
+        
